72862ec0d42001000473b3e3e686766fe27e69d2
Jamal Amar Bahena <jamal@Jamals-MacBook-Pro.local>
2019-01-31 11:12:32 -0600

    required express, pg and used port 8080

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const express = require('express')
+const {Client} = require('pg')
+const app = express()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const PORT = 8080
+app.listen(PORT, ()=> {
+	console.log('Listeting on port: ', PORT)
+})
+
